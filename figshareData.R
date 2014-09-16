6f66c6f4068eb3e8974bab88bde1687fbf873b21
erich <erich@mac.com>
2014-09-16 11:46:16 -0400

    Starting Synapse vignette script.

--- figshareData.R
+++ figshareData.R
@@ -26,4 +26,28 @@ install_git('git@github.com:ropensci/rfigshare.git')
 require(rfigshare)
 figAuth <- fs_auth() 
 
-# the package will now open a browser window
+# the package will now open a browser window from which you can
+# create a Figshare account and authenticate, the package will
+# then create a local file to cache credentials.
+
+## CREATE A FIGSHARE PROJECT
+myProjectID <- fs_create(title = 'My Test Figshare Project', 
+                         description = 'A test project for learning about Figshare', 
+                         type = 'fileset')
+
+# if you're properly authenticated, you'll get a retrun that states
+# that your 'article' has been created and it's ID number.
+
+## UPLOAD DATA TO FIGSHARE USING ITS API
+# for demo purposes let's just use the famous Iris dataset that 
+# comes with every R installation.
+data(iris)
+
+# convert to a .CSV file
+write.csv(iris, 'iris.csv')
+
+# send up to Figshare cloud
+uploadReturn <- fs_update(myProjectID, 'iris.csv')
+
+## PULL DOWN THE SAME ARTICLE
+myArticle <- fs_download(myProjectID)
---
+++ synapseData.R
@@ -0,0 +1,2 @@
+## synapseData.R
+
